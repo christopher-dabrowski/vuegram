31798ec9960f1bd42f8c610a813a78d58bcdc435
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-08-23 09:25:59 +0200

    Add placeholder view

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import(/* webpackChunkName: "settings" */ '../views/Settings.vue')
   }
 ]
 
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<script>
+console.log(process.env)
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

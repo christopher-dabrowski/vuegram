1a425c25e9df7c52008fcd377be7a41cb0e06941
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-08-25 20:54:07 +0200

    Signup form

--- src/store/operations.js
+++ src/store/operations.js
@@ -1,10 +1,12 @@
 const SET_USER_PROFILE = 'setUserProfile'
 
 const LOGIN = 'login'
+const SIGNUP = 'signup'
 const FETCH_USER_PROFILE = 'fetchUserProfile'
 
 export {
     SET_USER_PROFILE,
     LOGIN,
+    SIGNUP,
     FETCH_USER_PROFILE
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,7 @@
         </p>
       </div>
       <div class="col2">
-        <form @submit.prevent>
+        <form v-if="showLoginForm" @submit.prevent>
           <h1>Welcome Back</h1>
           <div>
             <label for="email1">Email</label>
@@ -35,7 +35,50 @@
           <button @click="login" class="button">Log In</button>
           <div class="extras">
             <a>Forgot Password</a>
-            <a>Create an Account</a>
+            <a @click="toggleForm()">Create an Account</a>
+          </div>
+        </form>
+        <form v-else @submit.prevent>
+          <h1>Get Started</h1>
+          <div>
+            <label for="name">Name</label>
+            <input
+              v-model.trim="signupForm.name"
+              type="text"
+              placeholder="Savvy Apps"
+              id="name"
+            />
+          </div>
+          <div>
+            <label for="title">Title</label>
+            <input
+              v-model.trim="signupForm.title"
+              type="text"
+              placeholder="Company"
+              id="title"
+            />
+          </div>
+          <div>
+            <label for="email2">Email</label>
+            <input
+              v-model.trim="signupForm.email"
+              type="text"
+              placeholder="you@email.com"
+              id="email2"
+            />
+          </div>
+          <div>
+            <label for="password2">Password</label>
+            <input
+              v-model.trim="signupForm.password"
+              type="password"
+              placeholder="min 6 characters"
+              id="password2"
+            />
+          </div>
+          <button @click="signup()" class="button">Sign Up</button>
+          <div class="extras">
+            <a @click="toggleForm()">Back to Log In</a>
           </div>
         </form>
       </div>
@@ -44,20 +87,40 @@
 </template>
 
 <script>
+import { LOGIN, SIGNUP } from "../store/operations";
+
 export default {
   data() {
     return {
+      showLoginForm: true,
       loginForm: {
+        email: "",
+        password: "",
+      },
+      signupForm: {
+        name: "",
+        title: "",
         email: "",
         password: "",
       },
     };
   },
   methods: {
+    toggleForm() {
+      this.showLoginForm = !this.showLoginForm;
+    },
     login() {
-      this.$store.dispatch("login", {
+      this.$store.dispatch(LOGIN, {
         email: this.loginForm.email,
         password: this.loginForm.password,
+      });
+    },
+    signup() {
+      this.$store.dispatch(SIGNUP, {
+        email: this.signupForm.email,
+        password: this.signupForm.password,
+        name: this.signupForm.name,
+        title: this.signupForm.title,
       });
     },
   },

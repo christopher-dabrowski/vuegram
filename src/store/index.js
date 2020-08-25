11cda247fe584db74dfcb9c4a3fb818fa5d05df5
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-08-25 20:46:11 +0200

    Login actions in Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as fb from '../firebase'
+import router from '../router/index'
+import { SET_USER_PROFILE, FETCH_USER_PROFILE } from './operations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userProfile: {}
   },
   mutations: {
+    setUserProfile(state, value) {
+      state.userProfile = value
+    }
   },
   actions: {
+    async login({ dispatch }, { email, password }) {
+      // sing user in
+      const { user } = await fb.auth.signInWithEmailAndPassword(email, password)
+
+      // fetch user profile and set it in state
+      dispatch(FETCH_USER_PROFILE, user)
+    },
+    async FETCH_USER_PROFILE({ commit }, user) {
+      // fetch user profile
+      const userProfile = await fb.usersCollection.doc(user.uid).get()
+
+      commit(SET_USER_PROFILE, userProfile.data())
+      router.push('/')
+    }
   },
   modules: {
   }
---
+++ src/store/operations.js
@@ -0,0 +1,10 @@
+const SET_USER_PROFILE = 'setUserProfile'
+
+const LOGIN = 'login'
+const FETCH_USER_PROFILE = 'fetchUserProfile'
+
+export {
+    SET_USER_PROFILE,
+    LOGIN,
+    FETCH_USER_PROFILE
+}

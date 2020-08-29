06b8403fa177eee267946a264cd32249dc873e92
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-08-29 14:47:03 +0200

    Navigation component

---
+++ src/components/SiteNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <section>
+      <div class="col1">
+        <router-link to="/">
+          <h3>Vuegram</h3>
+        </router-link>
+        <ul class="inline">
+          <li>
+            <router-link to="/">Dashboard</router-link>
+          </li>
+          <li>
+            <router-link to="/settings">Settings</router-link>
+          </li>
+          <li><a @click="logout">logout</a></li>
+        </ul>
+      </div>
+    </section>
+  </header>
+</template>
+
+<script>
+import { LOGOUT } from "../store/operations";
+
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch(LOGOUT);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

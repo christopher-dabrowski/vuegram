71784b60a24edbf4a609708b9d0c741eba88720f
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-10-06 19:08:30 +0200

    Dashboard screen

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,45 @@
 <template>
-  <h1>Dashboard</h1>
+  <div id="dashboard">
+    <section>
+      <div class="col1">
+        <div class="profile">
+          <h5>{{ userProfile.name }}</h5>
+          <p>{{ userProfile.title }}</p>
+          <div class="create-post">
+            <p>create a post</p>
+            <form @submit.prevent>
+              <textarea v-model.trim="post.content"></textarea>
+              <button :disabled="!post.content" class="button">
+                post
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col2">
+        <div>
+          <p class="no-results">There are currently no posts</p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-console.log(process.env)
-export default {
+import { mapState } from "vuex";
 
-}
+export default {
+  data() {
+    return {
+      post: {
+        content: "",
+      },
+    };
+  },
+  computed: {
+    ...mapState(["userProfile"]),
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

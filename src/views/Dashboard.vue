fb19ee476e35d35340a8e816fec185f009cb7a7e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-10-07 20:11:30 +0200

    Option to view full post

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,6 +8,39 @@
       />
     </transition>
 
+    <!-- full post modal -->
+    <transition name="fade">
+      <div v-if="showPostModal" class="p-modal">
+        <div class="p-container">
+          <a @click="closePostModal()" class="close">close</a>
+          <div class="post">
+            <h5>{{ fullPost.userName }}</h5>
+            <span>{{ fullPost.createdOn | formatDate }}</span>
+            <p>{{ fullPost.content }}</p>
+            <ul>
+              <li>
+                <a>comments {{ fullPost.comments }}</a>
+              </li>
+              <li>
+                <a>likes {{ fullPost.likes }}</a>
+              </li>
+            </ul>
+          </div>
+          <div v-show="postComments.length" class="comments">
+            <div
+              v-for="comment in postComments"
+              :key="comment.id"
+              class="comment"
+            >
+              <p>{{ comment.userName }}</p>
+              <span>{{ comment.createdOn | formatDate }}</span>
+              <p>{{ comment.content }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+
     <section>
       <div class="col1">
         <div class="profile">
@@ -43,7 +76,7 @@
               <li>
                 <a @click="likePost(post.id)">likes {{ post.likes }}</a>
               </li>
-              <li><a>view full post</a></li>
+              <li><a @click="viewPost(post)">view full post</a></li>
             </ul>
           </div>
         </div>
@@ -59,6 +92,7 @@
 import { mapState } from "vuex";
 import { CREATE_POST, LIKE_POST } from "../store/operations";
 import moment from "moment";
+import { commentsCollection } from "@/firebase";
 
 import CommentModal from "@/components/CommentModal";
 
@@ -73,6 +107,9 @@ export default {
       },
       showCommentModal: false,
       selectedPost: null,
+      showPostModal: false,
+      fullPost: null,
+      postComments: null,
     };
   },
   computed: {
@@ -91,6 +128,27 @@ export default {
     toggleCommentModal(post) {
       this.showCommentModal = !this.showCommentModal;
       this.selectedPost = post;
+    },
+
+    closePostModal() {
+      this.postComments = null;
+      this.showPostModal = false;
+    },
+
+    async viewPost(post) {
+      const comments = await commentsCollection
+        .where("postId", "==", post.id)
+        .get();
+
+      this.postComments = [];
+      comments.forEach((doc) => {
+        let comment = doc.data();
+        comment.id = doc.id;
+        this.postComments.push(comment);
+      });
+
+      this.fullPost = post;
+      this.showPostModal = true;
     },
   },
   filters: {
